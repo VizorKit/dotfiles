8075b94ca04ea13e2549b55f014ea689729bbbb1
Vizorkit <christopher.clark@vizorkit.com>
2020-09-27 21:08:50 -0500

    included clangd and emacs install

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt update
+sudo apt install emacs27
+sudo apt install build-essential
+sudo apt install clangd
