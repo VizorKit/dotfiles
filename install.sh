589497293f2d6d26d63ab3bdfe40559368a90628
vizorkit <christopher.clark@vizorkit.com>
2021-01-01 12:38:21 -0600

    adding rust support

--- install.sh
+++ install.sh
@@ -7,7 +7,27 @@ curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/
 chmod +x ~/.local/bin/rust-analyzer
 
 sudo apt install openocd
+export PATH=/home/vizorkit/.local/bin:$PATH 
 
 rustup default nightly
 rustup target add riscv32-unknown-none-elf
 cargo install cargo-binutils
+
+sudo apt install git
+sudo apt install python2
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python2 get-pip.py
+python2 -m pip
+
+sudo apt-get install build-essential zlib1g-dev pkg-config libglib2.0-dev binutils-dev libboost-all-dev autoconf libtool libssl-dev libpixman-1-dev libpython2-dev python-capstone virtualenv
+sudo apt install autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev \
+                 gawk build-essential bison flex texinfo gperf libtool patchutils bc \
+                 zlib1g-dev libexpat-dev git
+cd ~/
+git clone https://github.com/qemu/qemu
+cd qemu
+git checkout v5.0.0
+./configure --target-list=riscv32-softmmu
+make -j $(nproc)
+sudo make install
+export PATH=/home/vizorkit/qemu/
